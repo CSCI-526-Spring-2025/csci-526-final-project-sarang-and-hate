4cc1c81ad4ce01dd37724fd20d9c441ead71ef9b
Sahil-Hate <66158359+Sahil-Hate@users.noreply.github.com>
2025-03-10 02:10:45 -0700

    Changing the loader for Level2

--- Assets/Scripts/BootLoader.cs
+++ Assets/Scripts/BootLoader.cs
@@ -1,32 +1,3 @@
-// // using UnityEngine;
-// // using UnityEngine.SceneManagement;
-
-// // public class BootLoader : MonoBehaviour
-// // {
-// //     void Awake()
-// //     {
-// //         // Reset the loading state when the game starts
-// //         if (!PlayerPrefs.HasKey("GameStarted"))
-// //         {
-// //             PlayerPrefs.DeleteKey("LoadingCompleted"); // Reset the loading state
-// //             PlayerPrefs.SetInt("GameStarted", 1); // Mark that the game has started
-// //             PlayerPrefs.Save();
-// //         }
-
-// //         // If we are in SampleScene and loading hasn't been done, go to LoadingScene
-// //         if (SceneManager.GetActiveScene().name == "SampleScene" && !HasLoadingHappened())
-// //         {
-// //             SceneManager.LoadScene("LoadingScene");
-// //         }
-// //     }
-
-// //     // Function to check if LoadingScene has already happened
-// //     private bool HasLoadingHappened()
-// //     {
-// //         return PlayerPrefs.GetInt("LoadingCompleted", 0) == 1;
-// //     }
-// // }
-
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -41,7 +12,7 @@ public class BootLoader : MonoBehaviour
         }
 
         // If the game is already in InstructionsScene or SampleScene, do nothing
-        if (SceneManager.GetActiveScene().name == "InstructionsScene" || SceneManager.GetActiveScene().name == "SampleScene")
+        if (SceneManager.GetActiveScene().name == "InstructionsScene" || SceneManager.GetActiveScene().name == "SampleScene" || SceneManager.GetActiveScene().name == "Scene2")
         {
             return;
         }
