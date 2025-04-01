b3c1849b9aa01e97bb6f9df27c00789ff9fd8ff0
vsoneji7 <vsoneji@usc.edu>
2025-04-01 07:26:05 -0700

    Menu Added
    Added Menu with Level selection on start

---
+++ Assets/Scripts/LevelSelectManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using TMPro;
+using System.Collections.Generic;
+
+
+public class LevelSelectManager : MonoBehaviour
+{
+    public void LoadLevel1()
+    {
+        Time.timeScale = 1; // Just in case coming from a paused scene
+        SceneManager.LoadScene("SampleScene"); // Update if scene name is different
+    }
+
+    public void LoadLevel2()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Scene2");
+    }
+
+    public void LoadLevel3()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Scene3");
+    }
+}
